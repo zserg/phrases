aff83815f80052a70aef303b20d8e1f881fec37b
zserg <zsergt3@gmail.com>
2017-03-31 16:30:24 +0300

    some styling

--- ruseng/static/ruseng/app.js
+++ ruseng/static/ruseng/app.js
@@ -1,27 +1,46 @@
 console.log("Hello");
-var count = 5;
+var count = 3;
 console.log("0");
 var state = 'show';
 
+jQuery.fn.visible = function() {
+      return this.css('visibility', 'visible');
+};
+
+jQuery.fn.invisible = function() {
+      return this.css('visibility', 'hidden');
+};
+
 function get_data (){
     console.log('get_data')
-    $('#answer').hide();
+    $('#answer').invisible();
     $.get({url: 'get_data/',
            data: {'count': count},
-           success: function (data){
-              console.log('get_data - success');
-              $('#answer').hide();
-              $('#question').text(data.data.one);
-              $('#answer').text(data.data.two);
-              $('#answer-button').text('Show');
-            }
-    });
+           success: new_text
+          });
 }
+
+function new_text (data) {
+    console.log('new_text');
+    $('#answer').invisible();
+    text = "";
+    for (let item of data.data.one) {
+      text+=("<p>"+item+"</p>");
+    }
+    $('#question').html(text);
+    text = "";
+    for (let item of data.data.two) {
+      text+=("<p>"+item+"</p>");
+    }
+    $('#answer').html(text);
+    $('#answer-button').text('Show');
+}
+
 
 function clickHandler (num) {
     console.log('show_answer');
     if (state == 'show') {
-      $('#answer').show();
+      $('#answer').visible();
       $('#answer-button').text('Next');
       state = 'next';
     }else{
